8da6944a63e3aabf2f0cc2f1a46ca7e65e35bdb1
Naeem-Al <naeemali58013@gmail.com>
2024-05-13 13:51:22 +0600

    add second button alert

--- cool.js
+++ cool.js
@@ -1,5 +1,12 @@
 var button = document.getElementById('myButton');
 
+var button1 = document.getElementById('firsyButton');
+
+
+
 button.addEventListener('click', function() {
   alert('I am finished my assignment');
 });
+button1.addEventListener('click', function() {
+    alert('I am finished my assignment');
+  });
