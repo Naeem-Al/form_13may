16cdbf5aca7deaa38d4357ac52c879d4edee0d5a
Naeem-Al <naeemali58013@gmail.com>
2024-05-13 13:54:30 +0600

    a

--- cool.js
+++ cool.js
@@ -7,6 +7,7 @@ var button1 = document.getElementById('firsyButton');
 button.addEventListener('click', function() {
   alert('I am finished my assignment');
 });
+
 button1.addEventListener('click', function() {
     alert('I am finished my assignment');
   });
